d7a3087d515362afe14fd4390535981481c3da61
walletkun <feilinpersonal@gmail.com>
2025-02-22 19:52:44 -0500

    Remove unnecessary settings from configuration file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,9 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env')) # read the .env file
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY') # secret key from env
 
