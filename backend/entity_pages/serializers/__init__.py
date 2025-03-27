a0122f36b173aef51e3c3ba6f78aefce5d767d9b
Mohammad Najm <128323124+Mnajm6201@users.noreply.github.com>
2025-03-27 04:57:13 -0400

    Added author page

--- backend/entity_pages/serializers/__init__.py
+++ backend/entity_pages/serializers/__init__.py
@@ -1,8 +1,10 @@
 # library/serializers/__init__.py
 from .book_detail_serializer import BookDetailSerializer
 from .book_page_serializer import BookPageSerializer
+from .author_page_serializer import AuthorPageSerializer
 
 __all__ = [
     'BookDetailSerializer',
     'BookPageSerializer',
+    'AuthorPageSerializer',
 ]
